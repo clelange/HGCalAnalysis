4ae205402981050bb002e35a8aed2496e47d2dc5
Clemens Lange <clemens.lange@cern.ch>
2016-10-28 13:01:20 +0200

    further plots, adding RMS and mean

--- plotTogether.py
+++ plotTogether.py
@@ -34,28 +34,36 @@ def main():
 
     histList2D = ["RecHitsClus_layers_energy_cumulative",
                   "RecHitsClus_layers_energy_relative",
-                  "RecHits_layers_energy"]
+                  "RecHits_layers_energy",
+                  "SimVsRecHits_frac_energy_EE_FH+BH_fullEta"]
 
     histList1D = ["RecHitsClus_layers_energy_1D_cumulative",
                   "RecHitsClus_layers_energy_1D_relative",
                   "RecHits_layers_energy_1D",
-                  "RecHits_layers_nHits"]
+                  "RecHits_layers_nHits",
+                  "SimVsRecHits_frac_energy_EE_fullEta"]
 
     histYTitles = {}
-    histYTitles["RecHitsClus_layers_energy_cumulative"] = "cumulative relative RecHits/SimCluster energy deposits"
+    histYTitles["RecHitsClus_layers_energy_cumulative"] = "cumulative sum of RecHits energy deposits/SimCluster energy"
     histYTitles["RecHitsClus_layers_energy_relative"] = "relative RecHits/SimCluster energy deposits per layer"
-    histYTitles["RecHits_layers_energy"] = "RecHits energy deposits [a.u.]"
+    histYTitles["RecHits_layers_energy"] = "number of RecHits energy deposits [a.u.]"
+    histYTitles["SimVsRecHits_frac_energy_EE_FH+BH_fullEta"] = "events"
     histYTitles["RecHitsClus_layers_energy_1D_cumulative"] = "cumulative relative RecHits/SimCluster energy deposits"
     histYTitles["RecHitsClus_layers_energy_1D_relative"] = "relative RecHits/SimCluster energy deposits per layer"
     histYTitles["RecHits_layers_energy_1D"] = "RecHits energy deposits [a.u.]"
     histYTitles["RecHits_layers_nHits"] = "RecHits number of hits [a.u.]"
+    histYTitles["SimVsRecHits_frac_energy_EE_fullEta"] = "events"
+
+    histXTitles = {}
+    histXTitles["SimVsRecHits_frac_energy_EE_fullEta"] = "summed RecHits energy fraction EE"
+    histXTitles["SimVsRecHits_frac_energy_EE_FH+BH_fullEta"] = "summed RecHits energy fraction EE vs. FH+BH"
 
     fileDict = {}
     for sampleName in samples2plot:
         fileDict[sampleName] = ROOT.TFile.Open("%s.root" % sampleName)
 
     firstPlot = True
-    myLegend = ROOT.TLegend(0.16,0.75,0.48,0.95)
+    myLegend = ROOT.TLegend(0.16, 0.75, 0.48, 0.95)
     myLegend.SetBorderSize(0)
     myLegend.SetFillStyle(0)
     ROOT.gStyle.SetPadTopMargin(0.05)
@@ -64,21 +72,30 @@ def main():
     ROOT.gStyle.SetPadRightMargin(0.02)
 
     c = ROOT.TCanvas("c", "c", 500, 500)
+    printMeanRMS = False
     for hist in histList2D:
         histDict2D = {}
         histStackpfX = ROOT.THStack("%s_stack_pfX" % hist, "")
         histStackpjX = ROOT.THStack("%s_stack_pjX" % hist, "")
         maxYprojX = 0
+        meanVecPfX = {}
+        meanVecPjX = {}
+        rmsVecPfX = {}
+        rmsVecPjX = {}
         for sampleName in samples2plot:
             histDict2D[hist] = fileDict[sampleName].Get(hist)
             profileX = histDict2D[hist].ProfileX("%s_%s_pfX" % (sampleName, hist))
             profileX.SetMarkerColor(sampleColours[sampleName])
             profileX.SetLineColor(sampleColours[sampleName])
             profileX.SetMarkerStyle(sampleMarkerStyle[sampleName])
+            meanVecPfX[sampleName] = profileX.GetMean()
+            rmsVecPfX[sampleName] = profileX.GetRMS()
             projX = histDict2D[hist].ProjectionX("%s_%s_pjX" % (sampleName, hist))
             projX.SetMarkerColor(sampleColours[sampleName])
             projX.SetLineColor(sampleColours[sampleName])
             projX.SetMarkerStyle(sampleMarkerStyle[sampleName])
+            meanVecPjX[sampleName] = projX.GetMean()
+            rmsVecPjX[sampleName] = projX.GetRMS()
             if projX.GetMaximum() > maxYprojX:
                 maxYprojX = projX.GetMaximum()
             if firstPlot:
@@ -88,29 +105,55 @@ def main():
             histStackpfX.Add(profileX)
             histStackpjX.Add(projX)
         histStackpfX.Draw("nostack,hist,p")
-        histStackpfX.GetXaxis().SetTitle("layer")
+        if (hist.find("layer") >= 0):
+            printMeanRMS = False
+            histStackpfX.GetXaxis().SetTitle("layer")
+        else:
+            printMeanRMS = True
+            histStackpfX.GetXaxis().SetTitle(histXTitles[hist])
         histStackpfX.GetYaxis().SetTitle(histYTitles[hist])
         histStackpfX.GetYaxis().SetTitleOffset(histStackpfX.GetYaxis().GetTitleOffset()*1.95)
         myLegend.Draw()
+        if printMeanRMS:
+            tBox = ROOT.TLatex()
+            tBox.SetTextSize(0.03)
+            tBox.SetTextFont(42)
+            for index in range(len(samples2plot)):
+                tBox.DrawLatexNDC(0.5, 0.9-index*0.04, "mean: %5.2f, RMS: %5.2f" % (meanVecPfX[samples2plot[index]], rmsVecPfX[samples2plot[index]]))
         firstPlot = False
         c.SaveAs("%s_pfX.pdf" % hist)
+        meanVec = {}
+        rmsVec = {}
         histStackpjX.Draw("nostack,hist,p")
-        histStackpjX.GetXaxis().SetTitle("layer")
+        if (hist.find("layer") >= 0):
+            histStackpjX.GetXaxis().SetTitle("layer")
+        else:
+            histStackpjX.GetXaxis().SetTitle(histXTitles[hist])
         histStackpjX.SetMaximum(maxYprojX*1.2)
         histStackpjX.GetYaxis().SetTitle(histYTitles[hist])
         histStackpjX.GetYaxis().SetTitleOffset(histStackpjX.GetYaxis().GetTitleOffset()*1.95)
         myLegend.Draw()
+        if printMeanRMS:
+            tBox = ROOT.TLatex()
+            tBox.SetTextSize(0.03)
+            tBox.SetTextFont(42)
+            for index in range(len(samples2plot)):
+                tBox.DrawLatexNDC(0.5, 0.9-index*0.04, "mean: %5.2f, RMS: %5.2f" % (meanVecPjX[samples2plot[index]], rmsVecPjX[samples2plot[index]]))
         c.SaveAs("%s_pjX.pdf" % hist)
 
     for hist in histList1D:
         histDict1D = {}
         histStack = ROOT.THStack("%s_stack" % hist, "")
         maxY = 0
+        meanVec = {}
+        rmsVec = {}
         for sampleName in samples2plot:
             histDict1D[hist] = fileDict[sampleName].Get(hist).Clone("%s%s" %(hist,sampleName))
             histDict1D[hist].SetMarkerColor(sampleColours[sampleName])
             histDict1D[hist].SetLineColor(sampleColours[sampleName])
             histDict1D[hist].SetMarkerStyle(sampleMarkerStyle[sampleName])
+            meanVec[sampleName] = histDict1D[hist].GetMean()
+            rmsVec[sampleName] = histDict1D[hist].GetRMS()
             if histDict1D[hist].GetMaximum() > maxY:
                 maxY = histDict1D[hist].GetMaximum()
             if firstPlot:
@@ -120,10 +163,21 @@ def main():
             histStack.Add(histDict1D[hist])
         histStack.Draw("nostack,hist,p")
         histStack.SetMaximum(maxY*1.2)
-        histStack.GetXaxis().SetTitle("layer")
+        if (hist.find("layer") >= 0):
+            printMeanRMS = False
+            histStack.GetXaxis().SetTitle("layer")
+        else:
+            printMeanRMS = True
+            histStack.GetXaxis().SetTitle(histXTitles[hist])
         histStack.GetYaxis().SetTitle(histYTitles[hist])
         histStack.GetYaxis().SetTitleOffset(histStack.GetYaxis().GetTitleOffset()*1.95)
         myLegend.Draw()
+        if printMeanRMS:
+            tBox = ROOT.TLatex()
+            tBox.SetTextSize(0.03)
+            tBox.SetTextFont(42)
+            for index in range(len(samples2plot)):
+                tBox.DrawLatexNDC(0.5, 0.9-index*0.04, "mean: %5.2f, RMS: %5.2f" % (meanVec[samples2plot[index]], rmsVec[samples2plot[index]]))
         firstPlot = False
         c.SaveAs("%s.pdf" % hist)
 
